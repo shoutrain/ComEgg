afb70eb861104aae0fc61bb2884198a923aeaf08
rafael <rafael.gu@gongfuxin.com>
2015-02-26 09:33:57 +0800

    Finished CSingalQueue

--- src/Common/CSignalQueue.cpp
+++ src/Common/CSignalQueue.cpp
@@ -1,47 +1,52 @@
 #include "CSignalQueue.h"
 
 #include "CMutex.h"
+#include "CAutoLock.h"
 #include "CSignal.h"
 
-CSignalQueue::CSignalQueue(const ub_4 maxNum, CMutex *mutex) {
+CSignalQueue::CSignalQueue(const ub_4 maxNum, CMutex *mutex)
+        : _totalNum(maxNum), _res(maxNum, this, mutex) {
     assert(maxNum);
-    _maxNum          = maxNum;
-    _signals         = new CSignal[_maxNum];
-    assert(_signals);
     _mutex = mutex;
-    _curPreparedItem = null_v;
-    _queueHead       = _queueTail = null_v;
 }
 
 CSignalQueue::~CSignalQueue() {
-    _DEL_ARR(_signals);
 }
 
 CSignal *CSignalQueue::prepare() {
-    ub_4 index = 0;
+    CSignal *signal = _res.allocate();
 
-    for (; index < _maxNum; index++) {
-        if (!_signals[index].isUsed()) {
-            if (_mutex) {
-                _mutex->lock();
-            }
+    if (null_v == signal) {
+        logCrit("There is no more signal resource for signal queue");
+    }
 
-            _curPreparedItem = &_signals[index];
-            _curPreparedItem->use(true_v);
+    return signal;
+}
 
-            return &_signals[index];
-        }
+none_ CSignalQueue::send(const CSignal *signal) {
+    assert(signal);
+    CAutoLock al(_mutex);
+
+    _queue.push(signal);
+}
+
+CSignal *CSignalQueue::receive() {
+    CAutoLock al(_mutex);
+
+    CSignal *signal = _queue.front();
+
+    if (null_v != signal) {
+        _queue.pop();
+
+        return signal;
     }
 
     return null_v;
 }
 
-none_ CSignalQueue::send() {
-    if (_mutex) {
-        _mutex->unlock();
+none_ CSignalQueue::complete(const CSignal *signal) {
+    assert(signal);
+    if (false_v == _res.reclaim(signal)) {
+        assert(false_v);
     }
-}
-
-CSignal *CSignalQueue::receive() {
-
 }
--- src/Common/CSignalQueue.h
+++ src/Common/CSignalQueue.h
@@ -6,65 +6,71 @@
 *
 * \author Rafael Gu(shoutrain.goo@gmail.com)
 * \version 1.0.0
-* \date 02/18/2015
+* \date 02/26/2015
 */
 
 #ifndef _C_SIGNAL_QUEUE_H_
 #define _C_SIGNAL_QUEUE_H_
 
 #include "CGlobal.h"
+#include "CResource.h"
+
+#include <queue>
 
 class CMutex;
-class Signal;
+
+class CSignal;
 
 /// \brief CSignalQueue can queue a lot of CSignal instances.
 ///
 /// CSignalQueue can avoid using of delete.
 /// Notice: This class can support multi-thread signal sending and
-/// single-thread signal receiving
+/// single-thread signal receiving.
 class CSignalQueue : CGlobal {
 public:
-    /// \brief The Constructor of CSignalQueue
+    /// \brief The Constructor of CSignalQueue.
     ///
-    /// \param maxNum The total number of CSignal instances you want to queue.
+    /// \param maxNum The total number of CSignal instances you want to put
+    /// into the queue.
     /// \param mutex Make the instance of CSignalQueue to support multi-thread
     /// if mutex is not null_v.
     CSignalQueue(const ub_4 maxNum, CMutex *mutex = null_v);
 
-    /// \brief The Destructor of CSignalQueue
+    /// \brief The Destructor of CSignalQueue.
     ~CSignalQueue();
 
-    /// \brief Prepare the signal if there are free CSignal objects
+    /// \brief Prepare the signal if there are free CSignal objects.
     ///
-    /// This function will lock the queue until you call send()
-    /// \return Obtain null_v is there is no more CSignal object
+    /// \return Obtain null_v is there is no more CSignal object. DON'T FORGET
+    /// to reclaim the signal by calling complete().
     CSignal *prepare();
 
-    /// \brief the signal prepared is ready
+    /// \brief Put the prepared signal into queue.
     ///
     /// Please call prepare() first before call this function
-    none_ send();
+    /// \param signal The signal returned by calling prepare(), and don't call
+    /// send twice or more for one signal.
+    none_ send(const CSignal *signal);
 
     /// \brief Obtain the next signal if there are new signals
     ///
     /// \return Obtain null_v is there is no more signal
     CSignal *receive();
 
+    /// \brief Reclaim the used signal.
+    ///
+    /// Any signal returned by calling receive() must call this function to
+    /// reclaim.
+    /// \param singal The signal returned by calling receive().
+    none_ complete(const CSignal *signal);
+
 private:
-    const ub_4 _maxNum;
-    CSignal    *_signals;
+    const ub_4 _totalNum;
     CMutex     *_mutex;
 
-    struct TSignalNode {
-        CSignal *prevSignal;
-        CSignal *curSignal;
-        CSignal *nextSignal;
-    };
+    CResource<CSignal, CSignalQueue> _res;
 
-    CSignal *_curPreparedItem;
-
-    TSignalNode _queueHead;
-    TSignalNode _queueTail;
+    std::queue<CSignal *> _queue;
 };
 
 #endif // _C_SIGNAL_QUEUE_H_
